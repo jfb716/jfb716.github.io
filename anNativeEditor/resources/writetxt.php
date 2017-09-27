4e7497b2e92e78bb70106c8f9d5c065951bfe9fa
John Black <john.f.black@icloud.com>
2017-09-27 11:28:44 -0400

    add IA meta tag

--- anNativeEditor/resources/writetxt.php
+++
@@ -1,9 +0,0 @@
-<?php
-$newData = htmlspecialchars_decode($_POST['d']);
-$openFile = fopen("antag.txt", "w");
-fwrite($openFile, $newData);
-fclose($openFile);
-
-$tagToDisplay = file_get_contents("antag.txt");
-echo trim($tagToDisplay);
-?>
